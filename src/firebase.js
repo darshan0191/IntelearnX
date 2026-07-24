d9479b1544b5702acf76a66b3b60ef5ea3352d1a
darshan0191 <darshanmane8946@gmail.com>
2026-07-24 21:39:36 +0530

    security: move Firebase credentials to env variables

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,14 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBd2zmq7Ak8ImImuYtAK7PM4GW7voxd8Gg",
-  authDomain: "intelearnx.firebaseapp.com",
-  databaseURL: "https://intelearnx-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "intelearnx",
-  storageBucket: "intelearnx.firebasestorage.app",
-  messagingSenderId: "713799714010",
-  appId: "1:713799714010:web:017ee20912826a3a69d1e3",
-  measurementId: "G-4KXE1VXHKN",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
